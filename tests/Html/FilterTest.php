74c65670e197d177c0566325b70bca82b6df6e3d
Malitta Nanayakkara <malitta@thinkcube.com>
2016-01-24 22:57:07 +0530

    Add tests

---
+++ tests/Html/FilterTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Devrtips\Listr\Tests\Html;
+
+use Devrtips\Listr\Tests\Setup;
+use Devrtips\Listr\Listr;
+use Devrtips\Listr\Filter\Filter;
+
+class FilterTest extends \PHPUnit_Framework_TestCase
+{
+    use Setup;
+
+    public function testGetFiltersGroup()
+    {
+       
+    }
+}
